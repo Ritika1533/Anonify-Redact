601d847f35d483ab67fc0263f36d93eca9458701
unknown <ritikakes33@gmail.com>
2025-04-02 21:13:38 +0530

    Anonify

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,7 +5,11 @@ class Config:
     DEBUG = False
     SECRET_KEY = 'your_secret_key'
     # Specify the path to your local spaCy model
-    SPACY_MODEL_PATH = os.path.abspath(os.path.join(os.getcwd(), './model-best'))
+   
+
+    SPACY_MODEL_PATH = "D:/Anonify-Readact/Anonify/backend/model-best"
+    print("Model path exists:", os.path.exists(SPACY_MODEL_PATH))
+    print("Contents of the directory:", os.listdir(SPACY_MODEL_PATH) if os.path.exists(SPACY_MODEL_PATH) else "Directory not found")
     UPLOAD_PATH = os.path.abspath(os.path.join(os.getcwd(), './uploads'))
     SAVE_PATH = os.path.abspath(os.path.join(os.getcwd(), './results'))
     
